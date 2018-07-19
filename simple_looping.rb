5e3370c0bd70f12e0bd5fffd61a9e420b2c1bd53
Gina Lee <ginalee1114@gmail.com>
2018-07-19 19:37:04 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,8 +2,11 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
   loop do
     puts phrase
+    counter += 1
+    break if counter == 
   end
 end
 
